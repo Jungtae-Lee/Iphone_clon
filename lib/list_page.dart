72a88ef285137eb34888d7ce8180619aab0c4bb0
FlutLab.io <>
2021-08-23 05:55:17 +0000

    work2

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class ToDo {
   bool isDone = false;
   String title;
+  String description;
 
-  ToDo(this.title);
+  ToDo(this.title, this.description);
 }
 
 class ListPage extends StatefulWidget {
@@ -15,12 +16,14 @@ class ListPage extends StatefulWidget {
 class _ListPageState extends State<ListPage> {
   final _items = <ToDo>[];
 
-  var _titleController = new TextEditingController();
+  final _titleController = new TextEditingController();
+  final _descController = new TextEditingController();
 
   void _addTodo(ToDo todo) {
     setState(() {
       _items.add(todo);
       _titleController.text = '';
+      _descController.text = ' ';
     });
   }
 
@@ -39,6 +42,7 @@ class _ListPageState extends State<ListPage> {
   @override
   void dispose() {
     _titleController.dispose();
+    _descController.dispose();
     super.dispose();
   }
 
@@ -58,12 +62,31 @@ class _ListPageState extends State<ListPage> {
               Row(
                 children: <Widget>[
                   Expanded(
-                    child: TextField(
-                      controller: _titleController,
+                    child: Container(
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(color: Colors.black45, borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _titleController,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                              hintText: '제목',
+                            ),
+                          ),
+                          TextField(
+                            controller: _descController,
+                            decoration: InputDecoration(
+                              hintText: '메모',
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   TextButton(
-                      onPressed: () => _addTodo(ToDo(_titleController.text)),
+                      onPressed: () => _addTodo(ToDo(_titleController.text, _descController.text)),
                       child: Text(
                         '추가하기',
                         style: TextStyle(color: Colors.white),
@@ -87,14 +110,24 @@ class _ListPageState extends State<ListPage> {
         onPressed: () => _deleteTodo(todo),
         icon: Icon(Icons.delete),
       ),
-      title: Text(
-        todo.title,
-        style: todo.isDone
-            ? TextStyle(
-                decoration: TextDecoration.lineThrough,
-                fontStyle: FontStyle.italic,
-              )
-            : null,
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            todo.title,
+            style: todo.isDone ? TextStyle(decoration: TextDecoration.lineThrough, fontStyle: FontStyle.italic, fontSize: 20.0, fontWeight: FontWeight.bold) : TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            todo.description,
+            style: todo.isDone
+                ? TextStyle(
+                    decoration: TextDecoration.lineThrough,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 12.0,
+                  )
+                : TextStyle(fontSize: 12.0),
+          ),
+        ],
       ),
     );
   }
